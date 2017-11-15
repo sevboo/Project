4a6bb5f5eef2cfcc1de30867b949206aa07b14f0
junhyeongkim <posa20000@naver.com>
2017-11-15 14:08:40 +0900

    171115 project(수정)

--- app/src/main/java/com/example/administrator/project/MainActivity.java
+++ app/src/main/java/com/example/administrator/project/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.administrator.project;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,10 +18,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, Runnable {
     Button btnGameStart;
+    Thread thread;
+    String sendDate = "HI Server";
+    String recvDate = "";
+    ProgressDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,8 +60,23 @@ public class MainActivity extends AppCompatActivity
         btnGameStart.setOnClickListener(new View.OnClickListener() { //게임 스타트 버튼 눌렀을때
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),GameStartActivity.class);
-                startActivity(intent);
+                timeThread();
+                if(recvDate == "44") //방이 가득찼을때
+                {
+                    AlertDialog.Builder alert = new AlertDialog.Builder(getApplicationContext());
+                    alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();     //닫기
+                        }
+                    });
+                    alert.setMessage("게임 자리가 다찼습니다. 다음게임 이용해주세요!!");
+                    alert.show();
+                }
+                else { // 게임 시작을 할때
+                    Intent intent = new Intent(getApplicationContext(), GameStartActivity.class);
+                    startActivity(intent);
+                }
             }
         });
     }
@@ -108,4 +137,55 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    @Override
+    public void run() { // 소켓통신과 통신 방에 자리가 있는지 여부 확인.
+        try{
+                //socket을 만든다
+                Socket socket = new Socket("192.168.100.138", 5011);
+                //출력용 스트림 생성
+                PrintWriter outputStream = new PrintWriter(socket.getOutputStream(), true);
+                //데이터 전송
+                outputStream.write(sendDate);
+                //outstream 버퍼 비우기
+                outputStream.flush();
+                    //데이터 받기용 입력슽트림 생성
+                    BufferedReader inputStream = new BufferedReader(
+                                new InputStreamReader(socket.getInputStream(), "utf-8"));
+                    //데이터 받기
+                    recvDate = (String)inputStream.readLine();
+                    Log.i("MYLOG", "read " + recvDate);
+
+                //소켓종료
+                socket.close();
+            }catch (Exception e){
+                e.printStackTrace();
+                Log.e("MYLOG", e.getMessage());
+            }
+    }
+    public void timeThread() {
+
+        dialog = new ProgressDialog(this);
+        dialog = new ProgressDialog(this);
+        dialog.setTitle("Wait...");
+        dialog.setMessage("Please wait while loading...");
+        dialog.setIndeterminate(true);
+        dialog.setCancelable(true);
+        dialog.show();
+        new Thread(new Runnable() {
+            public void run() {
+                // TODO Auto-generated method stub
+                try {
+                    thread = new Thread(this);
+                    thread.start();
+                    Thread.sleep(5000);
+                } catch (Throwable ex) {
+                    ex.printStackTrace();
+                }
+                dialog.dismiss();
+            }
+        }).start();
+    }
 }
+
+
